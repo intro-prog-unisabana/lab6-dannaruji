d845704587871a14f6e254e15194278d4d560df5
Danna Ruiz <dannaruji@unisabana.edu.co>
2026-04-07 22:15:08 -0500

    Lab6 7

--- dict_func.py
+++ dict_func.py
@@ -1,1 +1,10 @@
 # Write your code here!
+def employee_print(employee_info):
+    Name = employee_info.get("Name", "N/A")
+    Salary = employee_info.get("Salary", "N/A")
+    Role = employee_info.get("Role", "N/A")
+
+    print(f"Name: {Name}")
+    print(f"Salary: {Salary}")
+    print(f"Role: {Role}")
+    
