ea45effbe57b177c482ade086c45f4b853e4025e
Danna Ruiz <dannaruji@unisabana.edu.co>
2026-04-07 21:59:18 -0500

    Lab6 3

--- dict_store.py
+++ dict_store.py
@@ -1,5 +1,5 @@
 # Write your code here!
-def temp_and_color(datos):
-    temperatura = datos.get("temperatura")
-    color = datos.get("color")
+def temp_and_color(data):
+    temperatura = data.get("temperatura")
+    color = data.get("color")
     return(temperatura, color)
