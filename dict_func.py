e6bb2be2d77573596ea4b91de941154a517e530c
Danna Ruiz <dannaruji@unisabana.edu.co>
2026-04-07 22:22:20 -0500

    Lab6 8

--- dict_func.py
+++ dict_func.py
@@ -7,4 +7,14 @@ def employee_print(employee_info):
     print(f"Name: {Name}")
     print(f"Salary: {Salary}")
     print(f"Role: {Role}")
-    
+
+    temp_dict = employee_info.copy()
+    temp_dict.pop("Name", None)
+    temp_dict.pop("Salary", None)
+    temp_dict.pop("Role", None)
+
+    if len(temp_dict) == 0:
+        print("No other info!")
+    else:
+        for key, value in temp_dict.items():
+            print(f"{key}: {value}")
