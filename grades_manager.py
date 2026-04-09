04af7440f8fe999552e2c248e1beb5132913c75f
Danna Ruiz <dannaruji@unisabana.edu.co>
2026-04-08 21:22:06 -0500

    Lab6 14

--- grades_manager.py
+++ grades_manager.py
@@ -10,7 +10,11 @@ def initialize_dict(student_name, subject_grades):
     
     return new_dict
 
-def add_student(student_grades={}):
+
+def add_student(student_grades=None):
+    if student_grades is None:
+        student_grades = {}
+    
     student_name = input("Enter student name: ").title()
     
     subjects = {}
@@ -26,9 +30,10 @@ def add_student(student_grades={}):
     
     student_grades[student_name] = subjects
     
-    print(f"Student {student_name} successfully added to the grades manager\n")
+    print(f"Student {student_name} successfully added to the grades management system.")
     
     return student_grades
+
 
 def get_students(student_grades, keys):
     result = {}
@@ -46,3 +51,23 @@ def get_students(student_grades, keys):
             print(f"{name.title()} not found!")
     
     return result
+
+def avg_by_student(student_grades, selected_students=None):
+    
+    if selected_students is None:
+        selected_students = list(student_grades.keys())
+    
+    for name in selected_students:
+        found = False
+        
+        for student in student_grades:
+            if student.lower() == name.lower():
+                grades = student_grades[student].values()
+                avg = round(sum(grades) / len(grades))
+                
+                print(f"{student}: {avg}")
+                found = True
+                break
+        
+        if not found:
+            print(f"{name.title()} not found!")
