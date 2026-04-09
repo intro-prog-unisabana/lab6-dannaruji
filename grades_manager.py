f184448d6c0e5a3674c80611a26eebdd036779a0
Danna Ruiz <dannaruji@unisabana.edu.co>
2026-04-08 21:13:54 -0500

    Lab6 13

--- grades_manager.py
+++ grades_manager.py
@@ -29,3 +29,20 @@ def add_student(student_grades={}):
     print(f"Student {student_name} successfully added to the grades manager\n")
     
     return student_grades
+
+def get_students(student_grades, keys):
+    result = {}
+    
+    for name in keys:
+        found = False
+        
+        for student in student_grades:
+            if student.lower() == name.lower():
+                result[student] = student_grades[student]
+                found = True
+                break
+        
+        if not found:
+            print(f"{name.title()} not found!")
+    
+    return result
