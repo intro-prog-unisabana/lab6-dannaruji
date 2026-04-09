f16d07e459fbde80df7a1f6efbefa6c3d0eb779a
Danna Ruiz <dannaruji@unisabana.edu.co>
2026-04-08 21:10:29 -0500

    Lab6 12

--- overall.py
+++ overall.py
@@ -1,19 +1,26 @@
 def student_averages(students):
     averages = {}
+    
     for student, grades in students.items():
         total = sum(grades.values())
-        promedio = round(total / len (grades))
-        averages[student] = promedio 
+        avg = round(total / len(grades))
+        averages[student] = avg
+    
+    return averages
+
 
 def assignment_averages(students):
     totals = {}
     counts = {}
+    
     for student in students:
         for task, grade in students[student].items():
             totals[task] = totals.get(task, 0) + grade
             counts[task] = counts.get(task, 0) + 1
-
+    
     averages = {}
+    
     for task in totals:
         averages[task] = round(totals[task] / counts[task])
+    
     return averages
